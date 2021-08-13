457a21f2346f8a7dc5a506a604c50116fd1d955f
wardxela <axelwardweb@gmail.com>
2021-08-13 09:41:50 +0300

    refactor: change queue to stack because of semantics

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 module.exports = function check(str, bracketsConfig) {
-  const queue = [];
+  const stack = [];
 
   for (let i = 0; i < str.length; i++) {
     for (let j = 0; j < bracketsConfig.length; j++) {
@@ -10,25 +10,25 @@ module.exports = function check(str, bracketsConfig) {
       } else if (index === 0) {
         // If brackets ara the same
         if (bracketsConfig[j][0] === bracketsConfig[j][1]) {
-          // If this is a closing one we remove opening bracket from the queue
-          if (queue[queue.length - 1] === str[i]) {
-            queue.pop();
+          // If this is a closing one we remove opening bracket from the stack
+          if (stack[stack.length - 1] === str[i]) {
+            stack.pop();
           } else {
-            queue.push(str[i]);
+            stack.push(str[i]);
           }
         } else {
-          queue.push(str[i]);
+          stack.push(str[i]);
         }
       } else if (index === 1) {
         // If a closing bracket doesn't have a corresponding opening one
-        // in the queue, we return false
-        if (!bracketsConfig[j].includes(queue.pop())) return false;
+        // in the stack, we return false
+        if (!bracketsConfig[j].includes(stack.pop())) return false;
       }
     }
   }
 
-  // Check whether there are left unclosed brackets in queue
-  if (queue.length) {
+  // Check whether there are left unclosed brackets in stack
+  if (stack.length) {
     return false;
   }
   return true;
